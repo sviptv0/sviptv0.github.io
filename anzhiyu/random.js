aff27299fea4722ecbfc6de2763e55b04d0259b7
sviptv0 <ps10@mail.com>
2024-08-07 14:09:15 +0800

    Site updated: 2024-08-07 14:09:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/06/hello-world/"];function toRandomPost(){
+var posts=["2024/08/06/docker搭建个人博客Wordpress/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
