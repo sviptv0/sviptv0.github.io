07bf83788fa0a902226fd61adfda4af821de42e8
sviptv0 <ps10@mail.com>
2024-08-07 18:14:03 +0800

    Site updated: 2024-08-07 18:14:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["搭建Xray证书自动续期/","docker搭建个人博客Wordpress/"];function toRandomPost(){
+var posts=["优选IP/","docker搭建个人博客Wordpress/","搭建Xray证书自动续期/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
