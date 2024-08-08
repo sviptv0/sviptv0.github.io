72442e9a1d4fd02b46d5b498bf019093a0ccb3de
sviptv0 <ps10@mail.com>
2024-08-08 12:16:35 +0800

    Site updated: 2024-08-08 12:16:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/39fc.html","posts/888c.html","posts/3e5.html"];function toRandomPost(){
+var posts=["undefined/39fc.html","undefined/888c.html","undefined/3e5.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
