e929770de3e032129a625a5b7a877429ec0c8d79
sviptv0 <ps10@mail.com>
2024-08-08 12:10:12 +0800

    Site updated: 2024-08-08 12:10:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/undefined.html","undefined/undefined.html","undefined/undefined.html"];function toRandomPost(){
+var posts=["undefined/39fc.html","undefined/3e5.html","undefined/888c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
