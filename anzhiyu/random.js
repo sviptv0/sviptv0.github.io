6dee8803b0598b2fc9e1b072ed13174b809465cf
sviptv0 <ps10@mail.com>
2024-08-08 11:54:49 +0800

    Site updated: 2024-08-08 11:54:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined.html/","posts/undefined.html/","posts/undefined.html/"];function toRandomPost(){
+var posts=["posts/优选IP.html","posts/docker搭建个人博客Wordpress.html","posts/搭建Xray证书自动续期.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
